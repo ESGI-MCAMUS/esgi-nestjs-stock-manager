baec4585aad461ad0944c3b0501a034dd9a50747
Milan Camus <pro@turtletv.fr>
2023-03-18 20:59:58 +0100

    ✨ Implement OpenAPI (Swagger)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ESGI Stock Manager')
+    .setDescription('Project for the ESGI course NestJS & TypeScript')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(Number(process.env.APP_PORT) ?? 3000);
 }
 bootstrap();
