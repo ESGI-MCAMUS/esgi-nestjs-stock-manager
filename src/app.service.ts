14f60bdef8cdc3cc4c9707cda5899da5c27e336b
Milan Camus <pro@turtletv.fr>
2023-03-19 19:27:01 +0100

    ✨ Implement new entities in the app

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World'
+  version(): object {
+    return { version: '1.0.0' };
   }
 }
