a92a01f1b8cb73536bf8a7053b4a87cc65535849
Milan Camus <pro@turtletv.fr>
2023-03-19 19:24:40 +0100

    ✨ Implement `Suppliers`

---
+++ src/suppliers/suppliers.entity.ts
@@ -0,0 +1,45 @@
+import {
+  BelongsTo,
+  Column,
+  ForeignKey,
+  HasMany,
+  HasOne,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { Product } from 'src/products/products.entity';
+import { User } from 'src/users/users.entity';
+
+@Table
+export class Supplier extends Model {
+  @Column
+  name: string;
+
+  @Column
+  address: string;
+
+  @Column
+  phone: string;
+
+  @Column
+  email: string;
+
+  @ForeignKey(() => User)
+  @Column
+  userId: number;
+
+  @BelongsTo(() => User)
+  user: User;
+
+  @HasMany(() => Product)
+  products: Product[];
+
+  @Column({ defaultValue: new Date() })
+  createdAt: Date;
+
+  @Column({ onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @Column({ defaultValue: null })
+  deletedAt: Date | null;
+}
---
+++ src/suppliers/suppliers.model.ts
@@ -0,0 +1,66 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from 'src/users/users.entity';
+import { Supplier } from './suppliers.entity';
+
+export interface CreateSupplier {
+  name: string;
+  description: string;
+  address: string;
+  phone: string;
+  email: string;
+  userId: number;
+}
+
+export type SupplierSearch = Partial<Supplier>;
+
+export class SupplierGetOne {
+  @ApiProperty({ type: 'number', format: 'binary' })
+  id: number;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  name: string;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  description: string;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  address: string;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  phone: string;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  email: string;
+  @ApiProperty({ type: 'number', format: 'binary' })
+  userId: number;
+}
+
+export class SupplierCreate {
+  @ApiProperty({ type: 'string', format: 'binary' })
+  name: string;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  description: string;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  address: string;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  phone: string;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  email: string;
+  @ApiProperty({ type: 'number', format: 'binary' })
+  userId: number;
+}
+
+export class SupplierUpdate {
+  @ApiProperty({ type: 'string', format: 'binary' })
+  name: string;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  description: string;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  address: string;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  phone: string;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  email: string;
+  @ApiProperty({ type: 'number', format: 'binary' })
+  user: number;
+}
+
+export class SupplierDelete {
+  @ApiProperty({ type: 'number', format: 'binary' })
+  id: number;
+}
