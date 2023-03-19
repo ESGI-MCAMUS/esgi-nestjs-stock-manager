ef1e37015fba50feb319c5a0feccdc0fb94875cc
Milan Camus <pro@turtletv.fr>
2023-03-19 19:24:21 +0100

    ✨ Implement `Products`

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,59 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { Product } from './products.entity';
+import {
+  ProductCreate,
+  ProductDelete,
+  ProductGetOne,
+  ProductSearch,
+} from './products.model';
+import { ProductsService } from './products.service';
 
 @ApiTags('Products')
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  async findAll(): Promise<Product[]> {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<Product> {
+    return this.productsService.findOne(id);
+  }
+
+  @Post()
+  @ApiBody({
+    description: 'Create a product',
+    type: ProductCreate,
+  })
+  @HttpCode(201)
+  async create(@Body() product: ProductCreate): Promise<Product> {
+    return this.productsService.create(product);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() product: Partial<Product>,
+  ): Promise<Product> {
+    return this.productsService.update(id, product);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async delete(@Param('id') id: number): Promise<Product> {
+    return this.productsService.delete(id);
+  }
+}
---
+++ src/products/products.entity.ts
@@ -0,0 +1,46 @@
+import {
+  BelongsTo,
+  BelongsToMany,
+  Column,
+  ForeignKey,
+  HasMany,
+  HasOne,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { Order, OrdersProducts } from 'src/orders/orders.entity';
+import { Supplier } from 'src/suppliers/suppliers.entity';
+
+@Table
+export class Product extends Model {
+  @Column
+  name: string;
+
+  @Column
+  description: string;
+
+  @Column
+  ean13: string;
+
+  @Column
+  price: number;
+
+  @ForeignKey(() => Supplier)
+  @Column
+  supplierId: number;
+
+  @BelongsTo(() => Supplier)
+  supplier: Supplier;
+
+  @BelongsToMany(() => Order, () => OrdersProducts)
+  orders: Order[];
+
+  @Column({ defaultValue: new Date() })
+  createdAt: Date;
+
+  @Column({ onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @Column({ defaultValue: null })
+  deletedAt: Date | null;
+}
