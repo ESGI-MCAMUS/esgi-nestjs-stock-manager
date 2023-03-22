d3939d421a76d03ba821aa5bb8ec6fdc8455cf26
Maxime MARCHAND <mmarchand6@myges.fr>
2023-03-22 11:11:26 +0100

    Rename folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,14 @@ import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
-import { ProductsService } from './products/products.service';
 import { OrdersController } from './orders/orders.controller';
-import { OrdersService } from './orders/orders.service';
 import { SuppliersController } from './suppliers/suppliers.controller';
-import { SuppliersService } from './suppliers/suppliers.service';
 import { AuthModule } from './auth/auth.module';
 import { AuthController } from './auth/auth.controller';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
-import { HttpExceptionFilter } from '../modules/sentry.middleware';
+import { HttpExceptionFilter } from '../middleware/sentry.middleware';
 @Module({
   imports: [AuthModule, OrdersModule, ProductsModule, SuppliersModule],
   controllers: [
