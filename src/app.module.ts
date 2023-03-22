54f49c0b274e132df95bba494cb18e6f310b07ed
Maxime MARCHAND <mmarchand6@myges.fr>
2023-03-22 02:35:33 +0100

    Feat: Sentry implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
@@ -12,6 +13,7 @@ import { AuthController } from './auth/auth.controller';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
+import { HttpExceptionFilter } from '../modules/sentry.middleware';
 @Module({
   imports: [AuthModule, OrdersModule, ProductsModule, SuppliersModule],
   controllers: [
@@ -21,6 +23,11 @@ import { SuppliersModule } from './suppliers/suppliers.module';
     ProductsController,
     SuppliersController,
   ],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }
