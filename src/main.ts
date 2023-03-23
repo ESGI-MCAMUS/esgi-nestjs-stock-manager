78b7dc080e81caecfa72f90c1a494aa55c315f69
Maxime MARCHAND <mmarchand6@myges.fr>
2023-03-23 01:21:46 +0100

    Activation de la gestion de CORS

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import helmet from 'helmet';
 require('dotenv').config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
