d1145969838abfc74d717f30bc3b2cc7898fc776
Maxime MARCHAND <mmarchand6@myges.fr>
2023-03-23 01:19:04 +0100

    Implémentation des en-têtes de sécurité avec Helmet

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as Sentry from '@sentry/node';
+import helmet from 'helmet';
+
 require('dotenv').config();
 
 async function bootstrap() {
@@ -18,6 +20,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  app.use(helmet());
+
   await app.listen(Number(process.env.APP_PORT) ?? 3000);
 }
 bootstrap();
