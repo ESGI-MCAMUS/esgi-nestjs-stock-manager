77db8588502eb7c114fe8626d9e4fd0d4e2bf4ef
Antoine <pollet.antoine.alexis@gmail.com>
2023-04-12 10:34:07 +0200

    Vaidations Supplier

--- src/suppliers/suppliers.model.ts
+++ src/suppliers/suppliers.model.ts
@@ -1,59 +1,64 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { User } from 'src/users/users.entity';
 import { Supplier } from './suppliers.entity';
-
+import { IsNumberString, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export interface CreateSupplier {
-  name: string;
-  address: string;
-  phone: string;
-  email: string;
-  userId: number;
+    name: string;
+    address: string;
+    phone: string;
+    email: string;
+    userId: number;
 }
 
 export type SupplierSearch = Partial<Supplier>;
 
 export class SupplierGetOne {
-  @ApiProperty({ type: 'number', format: 'binary' })
-  id: number;
-  @ApiProperty({ type: 'string', format: 'binary' })
-  name: string;
-  @ApiProperty({ type: 'string', format: 'binary' })
-  address: string;
-  @ApiProperty({ type: 'string', format: 'binary' })
-  phone: string;
-  @ApiProperty({ type: 'string', format: 'binary' })
-  email: string;
-  @ApiProperty({ type: 'number', format: 'binary' })
-  userId: number;
+    @ApiProperty({ type: 'number', format: 'binary' })
+    @IsNumberString()
+    @IsNotEmpty()
+    id: number;
+    @ApiProperty({ type: 'string', format: 'binary' })
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+    @ApiProperty({ type: 'string', format: 'binary' })
+    @IsNotEmpty()
+    @IsString()
+    address: string;
+    @ApiProperty({ type: 'string', format: 'binary' })
+    phone: string;
+    @ApiProperty({ type: 'string', format: 'binary' })
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+    @ApiProperty({ type: 'number', format: 'binary' })
+    userId: number;
 }
 
 export class SupplierCreate {
-  @ApiProperty({ type: 'string', format: 'binary' })
-  name: string;
-  @ApiProperty({ type: 'string', format: 'binary' })
-  address: string;
-  @ApiProperty({ type: 'string', format: 'binary' })
-  phone: string;
-  @ApiProperty({ type: 'string', format: 'binary' })
-  email: string;
-  @ApiProperty({ type: 'number', format: 'binary' })
-  userId: number;
+    @ApiProperty({ type: 'string', format: 'binary' })
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+    @ApiProperty({ type: 'string', format: 'binary' })
+    @IsString()
+    @IsNotEmpty()
+    address: string;
+    @ApiProperty({ type: 'string', format: 'binary' })
+    @IsString()
+    phone: string;
+    @ApiProperty({ type: 'string', format: 'binary' })
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+    @ApiProperty({ type: 'number', format: 'binary' })
+    userId: number;
 }
 
-export class SupplierUpdate {
-  @ApiProperty({ type: 'string', format: 'binary' })
-  name: string;
-  @ApiProperty({ type: 'string', format: 'binary' })
-  address: string;
-  @ApiProperty({ type: 'string', format: 'binary' })
-  phone: string;
-  @ApiProperty({ type: 'string', format: 'binary' })
-  email: string;
-  @ApiProperty({ type: 'number', format: 'binary' })
-  user: number;
-}
-
+export class SupplierUpdate extends PartialType(SupplierCreate) { }
 export class SupplierDelete {
-  @ApiProperty({ type: 'number', format: 'binary' })
-  id: number;
+    @ApiProperty({ type: 'number', format: 'binary' })
+    @IsNotEmpty()
+    @IsNumberString()
+    id: number;
 }
